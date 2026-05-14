1b80244c28987a78443a98d0bc10bff043508cc1
Uallace Macedo <uallace.macedo@outlook.com>
2026-05-14 13:34:07 -0300

    feat: change role type

--- livebit-api/src/main/resources/db/migration/V1__create_users_table.sql
+++ livebit-api/src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,11 +1,10 @@
 CREATE EXTENSION "uuid-ossp";
-CREATE TYPE USER_ROLE AS ENUM('user', 'admin');
 
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(40) NOT NULL UNIQUE,
     email VARCHAR(150) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role USER_ROLE NOT NULL default 'user',
+    role VARCHAR(10) NOT NULL default 'user',
     created_at TIMESTAMP NOT NULL default current_timestamp
 );
