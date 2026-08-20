b9a3e3411c2b4a4a84a13509578eede2806b3a4c
Uallace Macedo <uallace.macedo@outlook.com>
2026-08-20 17:17:01 -0300

    feat(profile): add actions drawer

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../components/Layout";
 import { HomePage } from "@/features/home/pages/HomePage";
+import { PAGES } from "@/constants/pages";
 
 
 export function AppRouter() {
@@ -9,14 +10,15 @@ export function AppRouter() {
       <Routes>
         {/* Public Routes */}
         <Route element={<Layout showSearchInHeader={true} />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/events" element={<h1>Lista de Eventos</h1>} />
-          <Route path="/account" element={<h1>Minha Conta</h1>} />
-          <Route path="/tickets" element={<h1>Meus Ingressos</h1>} />
+          <Route path={PAGES.PUBLIC.HOME} element={<HomePage />} />
+          <Route path={PAGES.PUBLIC.EVENTS} element={<h1>Lista de Eventos</h1>} />
         </Route>
 
+        {/* Private Routes */}
         <Route element={<Layout showSearchInHeader={false} />}>
           <Route path="/events/:id" element={<h1>Detalhes do Evento</h1>} />
+          <Route path={PAGES.PRIVATE.PROFILE} element={<h1>Minha Conta</h1>} />
+          <Route path={PAGES.PRIVATE.TICKETS} element={<h1>Meus Ingressos</h1>} />
         </Route>
       </Routes>
     </BrowserRouter>
