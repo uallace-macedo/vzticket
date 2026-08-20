d1b756752542b178129cfd01e05ddfd9b1a6882f
Uallace Macedo <uallace.macedo@outlook.com>
2026-08-20 10:09:39 -0300

    feat(api): add cors middleware

--- apps/api/vzticket/main.py
+++ apps/api/vzticket/main.py
@@ -1,11 +1,14 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from vzticket.core.exceptions import register_exception_handlers
 from vzticket.core.exceptions.schemas import ValidationErrorResponse
+from vzticket.core.settings import settings
 from vzticket.router import api_router
 
+origins = [settings.WEB_URL]
 app = FastAPI(
     title='EliteDEV Verzel - API',
     description='API da Plataforma de Eventos e Ingressos',
@@ -17,6 +20,13 @@ app = FastAPI(
     },
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 register_exception_handlers(app)
