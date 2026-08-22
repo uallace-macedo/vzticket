b212cf4a0146a524fbbb4bf0da63daf2c2b10934
Uallace Macedo <uallace.macedo@outlook.com>
2026-08-21 22:08:07 -0300

    feat(wctokens): extends model with target and type

--- apps/api/vzticket/modules/wallet_claim_tokens/model.py
+++ apps/api/vzticket/modules/wallet_claim_tokens/model.py
@@ -1,10 +1,11 @@
-import enum
 import uuid
 from datetime import datetime
 from decimal import Decimal
+from enum import Enum
 from typing import TYPE_CHECKING, Optional
 
-from sqlalchemy import DateTime, Enum, ForeignKey, Numeric, String
+from sqlalchemy import DateTime, ForeignKey, Numeric, String
+from sqlalchemy import Enum as SQLEnum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from vzticket.core.database import table_registry
@@ -13,10 +14,16 @@ if TYPE_CHECKING:
     from vzticket.modules.users.model import User
 
 
-class ClaimTokenStatus(str, enum.Enum):
-    PENDING = 'PENDING'
-    CLAIMED = 'CLAIMED'
-    EXPIRED = 'EXPIRED'
+class ClaimTokenStatus(str, Enum):
+    PENDING = 'pending'
+    CLAIMED = 'claimed'
+    EXPIRED = 'expired'
+
+
+class ClaimType(str, Enum):
+    DEPOSIT = 'deposit'
+    TICKET_PURCHASE = 'ticket_purchase'
+    EVENT_FEE = 'event_fee'
 
 
 @table_registry.mapped_as_dataclass
@@ -35,20 +42,28 @@ class WalletClaimToken:
         index=True,
         nullable=False
     )
+
     amount: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
-    status: Mapped[ClaimTokenStatus] = mapped_column(
-        Enum(ClaimTokenStatus),
-        init=False,
-        default=ClaimTokenStatus.PENDING, nullable=False
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False
     )
 
     expires_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         nullable=False
     )
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
+
+    type: Mapped[ClaimType] = mapped_column(
+        SQLEnum(ClaimType, native_enum=False),
+        default=ClaimType.DEPOSIT,
         nullable=False
+    )
+
+    target_id: Mapped[Optional[uuid.UUID]] = mapped_column(
+        default=None,
+        nullable=True
     )
 
     user_id: Mapped[Optional[uuid.UUID]] = mapped_column(
@@ -57,10 +72,18 @@ class WalletClaimToken:
         nullable=True,
         index=True
     )
+
     claimed_at: Mapped[Optional[datetime]] = mapped_column(
         DateTime(timezone=True),
         default=None,
         nullable=True
+    )
+
+    status: Mapped[ClaimTokenStatus] = mapped_column(
+        SQLEnum(ClaimTokenStatus, native_enum=False),
+        init=False,
+        default=ClaimTokenStatus.PENDING,
+        nullable=False
     )
 
     user: Mapped[Optional['User']] = relationship(init=False)
